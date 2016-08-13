bfd6a6ad4b0f3bccb6e2d4e9129bc6371a0068f4
zebult <saichi.suzuki@gmail.com>
2016-08-14 00:30:39 +0900

    code clean up

--- SimpleScrollViewPaging/ViewController.swift
+++ SimpleScrollViewPaging/ViewController.swift
@@ -9,42 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController{
-    
     private var scrollView: UIScrollView!
     
     override func viewDidLoad() {
-        
-        // ビューの縦、横のサイズを取得する.
-        let width = self.view.frame.maxX, height = self.view.frame.maxY
-        
-        // 背景の色をCyanに設定する.
-        self.view.backgroundColor = UIColor.cyanColor()
-        
-        // ScrollViewを取得する.
-        scrollView = UIScrollView(frame: self.view.frame)
-        
-        // ページ数を定義する.
         let pageSize = 4
-        
+        let width = self.view.frame.maxX, height = self.view.frame.maxY
+        scrollView = UIScrollView(frame: self.view.frame)
         // 縦方向と、横方向のインディケータを非表示にする.
         scrollView.showsHorizontalScrollIndicator = false;
         scrollView.showsVerticalScrollIndicator = false
-        
         // ページングを許可する.
         scrollView.pagingEnabled = true
-        
         // ScrollViewのデリゲートを設定する.
         //        scrollView.delegate = self
-        
         // スクロールの画面サイズを指定する.
         scrollView.contentSize = CGSizeMake(CGFloat(pageSize) * width, 0)
-        
         // ScrollViewをViewに追加する.
         self.view.addSubview(scrollView)
 
         // ページ数分ボタンを生成する.
-        for var i = 0; i < pageSize; i++ {
-            
+        for i in 0 ..< pageSize {
             // ページごとに異なるラベルを生成する.
             let myLabel:UILabel = UILabel(frame: CGRectMake(CGFloat(i) * width + width/2 - 40, height/2 - 40, 80, 80))
             myLabel.backgroundColor = UIColor.blackColor()
@@ -54,11 +38,7 @@ class ViewController: UIViewController{
             myLabel.text = "Page\(i)"
             myLabel.font = UIFont.systemFontOfSize(UIFont.smallSystemFontSize())
             myLabel.layer.cornerRadius = 40.0
-            
             scrollView.addSubview(myLabel)
         }
-        
     }
-    
-    
 }
